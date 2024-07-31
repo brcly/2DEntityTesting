b4132c50ce2571ed78674cf16362080c8fcd94a3
Brcly <barclay@tuta.io>
2024-07-31 01:13:31 +0100

    Refactored code
    Changed a lot.
    
    Still nothing to talk about. Still not ready for anything.

--- Scripts/Player/player.gd
+++ Scripts/Player/player.gd
@@ -1,5 +1,21 @@
-class_name player extends Entity
+class_name Player
 
+extends Entity
+
+var saved_state
+
+func _process(delta: float) -> void:
+	velocity = direction * speed * delta
+	if direction != Vector2.ZERO and current_state != State.ATTACKING:
+		switchState(State.MOVING)
+	elif direction == Vector2.ZERO:
+		switchState(State.IDLE)
+
+func _input(event: InputEvent) -> void:
+	direction = Input.get_vector("left", "right", "up", "down")
+	if Input.is_action_just_pressed("ui_accept"):
+		saved_state = current_state
+		switchState(State.ATTACKING)
 
 func _on_animation_tree_animation_finished(anim_name: StringName) -> void:
-	isAttacking = false
+	switchState(saved_state)
--- Scripts/entity.gd
+++ Scripts/entity.gd
@@ -1,82 +1,67 @@
-class_name Entity extends PhysicsBody2D
+class_name Entity 
 
-@export var entityName : String = ""
-@export var godMode : bool = false
-@export var health : int = 100
-@export var sprite : AnimatedSprite2D
-@export var canMove : bool = false
-@export var speed : int = 1
-@export var canAttack : bool = false
-@export var canAutoHeal : bool = false
-@export var controllable : bool = false
-@export var collisionBox : CollisionShape2D
-@export var hurtBox : Area2D
-@export var hitBox : Area2D
+extends PhysicsBody2D
+
+@export var entity_name : String = ""
+@export var god_mode : bool = false
+@export var max_health : int = 100
+@export var can_move : bool = false
+@export var speed : int = 100
 @export var isAnimated : bool = false
-@export var loadAnimTree : AnimationTree
-@export var animPlayer : AnimationPlayer
 
-@onready var animTree = loadAnimTree
+@export var anim_tree : AnimationTree
+@export var sprite : AnimatedSprite2D
 
-var direction : Vector2 = Vector2.ZERO
-var isAttacking = false
-#
+var current_state : State
+var current_health : int = max_health
+var velocity : Vector2
+var direction : Vector2
+
+enum State {IDLE, MOVING, ATTACKING}
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	if self.isAnimated:
-		animTree.get("parameters/playback").travel("Idle", true)
+	if isAnimated:
+		anim_tree.get("parameters/playback").travel("Idle", true)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	var velocity = direction * speed
-	if controllable:
-		get_input()
 	if isAnimated:
-		set_animations(isAttacking)
-	if canMove:
+		set_animations(current_state, direction)
+	if can_move:
 		move_and_collide(velocity)
-	
-func get_input():
-	direction = Input.get_vector("left", "right", "up", "down")
-	if Input.is_action_just_pressed("ui_accept"):
-		isAttacking = true
-	
-func set_animations(attacking = false):
-	if direction == Vector2.ZERO:
-		if animTree.get("parameters/playback").get_current_node() != "Idle":
-			animTree.get("parameters/playback").travel("Idle")
-	elif !attacking:
-		if animTree.get("parameters/playback").get_current_node() != "Walk":
-			animTree.get("parameters/playback").travel("Walk")
-		animTree.set("parameters/Attack/BlendSpace2D/blend_position", direction)
-		animTree.set("parameters/Idle/BlendSpace2D/blend_position", direction)
-		animTree.set("parameters/Walk/BlendSpace2D/blend_position", direction)
-	elif attacking:
-		if animTree.get("parameters/playback").get_current_node() != "Attack":
-			animTree.get("parameters/playback").travel("Attack")
 		
+func switchState(newState : State):
+	match newState:
+		State.IDLE:
+			current_state = State.IDLE
+		State.MOVING:
+			current_state = State.MOVING
+		State.ATTACKING:
+			current_state = State.ATTACKING
 
-func get_health() -> int:
-	return self.health
-	
-func set_health(newHealth : int):
-	self.health = newHealth
-	
-func get_speed() -> int:
-	return self.speed
+func set_animations(state : State, direction : Vector2):
+	match state:
+		State.ATTACKING:
+			if anim_tree.get("parameters/playback").get_current_node() != "Attack":
+				anim_tree.get("parameters/playback").travel("Attack")
+		State.IDLE:
+			if anim_tree.get("parameters/playback").get_current_node() != "Idle":
+				anim_tree.get("parameters/playback").travel("Idle")
+		State.MOVING:
+			if anim_tree.get("parameters/playback").get_current_node() != "Walk":
+				anim_tree.get("parameters/playback").travel("Walk")
+			anim_tree.set("parameters/Attack/BlendSpace2D/blend_position", direction)
+			anim_tree.set("parameters/Idle/BlendSpace2D/blend_position", direction)
+			anim_tree.set("parameters/Walk/BlendSpace2D/blend_position", direction)
 
-func get_canMove() -> bool:
-	return self.canMove
+func apply_damage(damage_amount : int):
+	current_health -= damage_amount
+	if current_health <= 0:
+		kill_entity()
 
-func set_canMove(newCanMove : bool):
-	self.canMove = newCanMove
+func apply_heal(heal_amount : int):
+	current_health += heal_amount
 
-func get_controllable() -> bool:
-	return controllable
-	
-func set_controllable(newControllable : bool):
-	self.controllable = newControllable 
-	
-func get_entityName() -> String:
-	return self.entityName
+func kill_entity():
+	queue_free()
